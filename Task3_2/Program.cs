ef7e0a47e107fca0e658905bddd8c498713e80c0
Pavel Znamensky <pavel2128506@gmail.com>
2025-03-05 14:39:06 +0300

    домашка 3 урок

--- Task3_2/Program.cs
+++ Task3_2/Program.cs
@@ -9,19 +9,33 @@ namespace Task3_2
             int a = Convert.ToInt32(Console.ReadLine());
             int b = Convert.ToInt32(Console.ReadLine());
             int c = Convert.ToInt32(Console.ReadLine());
-            switch (a)
+            //switch (a)
+            //{
+                //case  >b && > c:    //тут возникает ошибка, не знаю как синтаксически правильно записать такое условие в case
+                    //Console.WriteLine('a');
+                    //break;
+                //case  >b && <c:
+                    //Console.WriteLine('c');
+                    //break;
+                //default:
+                    //Console.WriteLine('b');
+                    //break;
+
+            //}
+
+            if (a > b && a > c)
             {
-                case (> b) when a > c:
-                    Console.WriteLine('a');
-                    break;
-                case (>b) when  a <c:
-                    Console.WriteLine('c');
-                    break;
-                case (< b) when a < c:
-                    Console.WriteLine('b');
-                    break;
+                Console.WriteLine('a');
             }
-            
+            else if (a > b && b < c || a < b && b < c)
+            {
+                Console.WriteLine('c');
+            }
+            else if (a < b && b > c)
+            {
+                Console.WriteLine('b');
+            }
+
             Console.ReadKey();
         }
     }
