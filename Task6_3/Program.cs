c699031566e971bbb4c3ef9c1c554687ce5ef047
Pavel Znamensky <pavel2128506@gmail.com>
2025-04-03 00:01:53 +0300

    Домашняя работа урок 6

---
+++ Task6_3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+namespace Task6_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string month = "сентябрь";
+            int year = 1995; 
+            double total_sum = 28732.35;
+            int count = 831;
+            double avg = total_sum / count;
+
+            Console.WriteLine(new String('-', 20));
+            Console.WriteLine("Отчет о продажах за {0} {1}", month, year);
+            Console.WriteLine(new String('-', 20));
+            Console.WriteLine("Общая сумма продаж : {0}",string.Format("{0:N2}", total_sum));
+            Console.WriteLine("Количество проданных товаров : {0}", count);
+            Console.WriteLine("Средняя стоимость товара : {0}", string.Format("{0:.00}", avg));
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
