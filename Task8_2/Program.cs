438c8ed00eeb3c3836664b82416c9832a31c9921
Pavel Znamensky <pavel2128506@gmail.com>
2025-05-14 01:44:15 +0300

    Домашнее задание Урок 8

---
+++ Task8_2/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Task8_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите возраст");
+            int age = Convert.ToInt32(Console.ReadLine());
+
+            try
+            {
+                ValidateAge(age);
+            }
+            catch (ArgumentException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+            Console.ReadKey();
+        }
+
+        static void ValidateAge(int age)
+        {
+            if (age < 0)
+                throw new ArgumentException("Возраст не может быть отрицательным!");
+
+            else if (age > 150)
+                throw new ArgumentException("Слишком большой возраст!");
+            else
+            {
+                Console.WriteLine("Корректный возраст");
+            }
+
+
+        }
+    }
+}
