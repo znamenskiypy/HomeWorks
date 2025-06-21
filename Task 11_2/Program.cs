8e0abbd3b5e6a47adf851a0592ecd4d03ca085fb
Pavel Znamensky <pavel2128506@gmail.com>
2025-06-21 15:55:46 +0300

    Домашка 11 урок

---
+++ Task 11_2/Program.cs
@@ -0,0 +1,59 @@
+﻿namespace Task_11_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            IFlyable[] objects = new IFlyable[]
+            {
+                    new Bird(15),
+                    new Airplane(5000,20),
+                    new Bird(500),
+                    new Airplane (1000,150)
+            };
+            foreach (var obj in objects)
+            {
+                obj.Fly();
+            }
+            Console.ReadKey();
+
+        }
+
+        public interface IFlyable
+        {
+            void Fly();
+            int MaxAltitude { get; }
+        
+        }
+
+        public class Bird : IFlyable
+        {
+            public int MaxAltitude { get; }
+            public Bird(int alt)
+            {
+                MaxAltitude = alt;
+
+            }
+            public void Fly()
+            {
+                Console.WriteLine($"Лечу на высоте {MaxAltitude} метров");
+
+            }
+        }
+        public class Airplane : IFlyable
+        {
+            public int MaxAltitude { get; }
+            public int CountPassengers { get; }
+            public Airplane(int alt, int countpas)
+            {
+                MaxAltitude = alt;
+                CountPassengers = countpas;
+            }
+            public void Fly()
+            {
+                Console.WriteLine($"Лечу на высоте {MaxAltitude} метров, везу {CountPassengers} пассажиров");
+
+            }
+        }
+    }
+}
